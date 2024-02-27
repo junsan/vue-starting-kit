7b230944858cc4fb192caeeec1779bbbe7da04da
Jun Santos <xjunsantos@gmail.com>
2024-02-27 15:55:34 +0800

    v-for

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -1,15 +1,26 @@
 <template>
     <ul>
-        <li>
-            <RouterLink to="/post-detail/1">Post 1</RouterLink>
-        </li>
-        <li>
-            <RouterLink to="/post-detail/2">Post 2</RouterLink>
-        </li>
-        <li>
-            <RouterLink to="/post-detail/3">Post 3</RouterLink>
+        <li v-for="post in posts" :key="post.id">
+            <RouterLink :to="`/post-detail/${post.id}`">{{ post.title }}</RouterLink>
         </li>
     </ul>
 </template>
-<script>
+<script setup>
+import { ref } from 'vue'
+
+const posts = ref([
+    {
+        id: 1,
+        title: 'Post 1'
+    },
+    {
+        id: 2,
+        title: 'Post 2'
+    },
+    {
+        id: 3,
+        title: 'Post 3'
+    }
+])
+
 </script>
